bc1c0e37f5166c41b16be563bedefb77ead8988c
Yaroslava <lanhraf.yaroslava@gmail.com>
2025-10-30 12:49:53 +0200

    task8_volumes

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,15 @@
 set -e
 echo "Deploying all Kubernetes resources..."
 
-kubectl apply -f .infrastructure/namespace.yml
-kubectl apply -f .infrastructure/configMap.yml
-kubectl apply -f .infrastructure/secret.yml
-kubectl apply -f .infrastructure/pv.yml
-kubectl apply -f .infrastructure/pvc.yml
-kubectl apply -f .infrastructure/clusterIp.yml
-kubectl apply -f .infrastructure/nodePort.yml
-kubectl apply -f .infrastructure/deployment.yml
-kubectl apply -f .infrastructure/hpa.yml
+kubectl apply -f namespace.yml
+kubectl apply -f configMap.yml
+kubectl apply -f secret.yml
+kubectl apply -f pv.yml
+kubectl apply -f pvc.yml
+kubectl apply -f clusterIp.yml
+kubectl apply -f nodePort.yml
+kubectl apply -f deployment.yml
+kubectl apply -f hpa.yml
 
 echo "Deployment complete!"
 
